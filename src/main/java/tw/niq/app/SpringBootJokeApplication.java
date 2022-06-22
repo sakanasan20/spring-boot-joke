277ca75ac87b743cee9c87e398c06df898957571
nick.chen <sakanasan20@gmail.com>
2022-06-22 11:44:42 +0800

    Initial commit

---
+++ src/main/java/tw/niq/app/SpringBootJokeApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJokeApplication.class, args);
+	}
+
+}
